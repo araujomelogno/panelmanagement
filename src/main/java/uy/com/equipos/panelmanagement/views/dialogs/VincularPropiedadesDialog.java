71274b4e91d3dbea66589fa7cd86078b7736d990
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-07-21 21:16:14 -0300

    horl

--- src/main/java/uy/com/equipos/panelmanagement/views/dialogs/VincularPropiedadesDialog.java
+++ src/main/java/uy/com/equipos/panelmanagement/views/dialogs/VincularPropiedadesDialog.java
@@ -22,112 +22,95 @@ import uy.com.equipos.panelmanagement.services.SurveyPropertyMatchingService;
 
 public class VincularPropiedadesDialog extends Dialog {
 
-	private final Survey survey;
-	private final Grid<PanelistProperty> grid = new Grid<>(PanelistProperty.class, false);
-	private final PanelistPropertyService panelistPropertyService;
-	private final SurveyPropertyMatchingService surveyPropertyMatchingService;
-	private final Map<PanelistProperty, Checkbox> checkboxMap = new HashMap<>();
-	private final Map<PanelistProperty, TextField> textFieldMap = new HashMap<>();
+    private final Survey survey;
+    private final Grid<PanelistProperty> grid = new Grid<>(PanelistProperty.class, false);
+    private final PanelistPropertyService panelistPropertyService;
+    private final SurveyPropertyMatchingService surveyPropertyMatchingService;
+    private final Map<PanelistProperty, Checkbox> checkboxMap = new HashMap<>();
+    private final Map<PanelistProperty, TextField> textFieldMap = new HashMap<>();
 
-	public VincularPropiedadesDialog(Survey survey, PanelistPropertyService panelistPropertyService,
-			SurveyPropertyMatchingService surveyPropertyMatchingService) {
-		this.survey = survey;
-		this.panelistPropertyService = panelistPropertyService;
-		this.surveyPropertyMatchingService = surveyPropertyMatchingService;
-		setHeaderTitle("Vincular Propiedades para: " + survey.getName());
-		setWidth("80%");
-		setHeight("70%");
+    public VincularPropiedadesDialog(Survey survey, PanelistPropertyService panelistPropertyService,
+            SurveyPropertyMatchingService surveyPropertyMatchingService) {
+        this.survey = survey;
+        this.panelistPropertyService = panelistPropertyService;
+        this.surveyPropertyMatchingService = surveyPropertyMatchingService;
+        setHeaderTitle("Vincular Propiedades para: " + survey.getName());
+        setWidth("80%");
+        setHeight("70%");
 
-		VerticalLayout dialogLayout = new VerticalLayout(grid);
-		dialogLayout.setSizeFull();
-		dialogLayout.setPadding(false);
-		dialogLayout.setSpacing(false);
-		dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        VerticalLayout dialogLayout = new VerticalLayout(grid);
+        dialogLayout.setSizeFull();
+        dialogLayout.setPadding(false);
+        dialogLayout.setSpacing(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
 
-		add(dialogLayout);
+        add(dialogLayout);
 
-		Button closeButton = new Button("Cerrar", e -> close());
-		closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-		Button saveButton = new Button("Guardar", e -> save());
-		saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		getFooter().add(closeButton, saveButton);
+        Button closeButton = new Button("Cerrar", e -> close());
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        Button saveButton = new Button("Guardar", e -> save());
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        getFooter().add(closeButton, saveButton);
 
-		setupGrid();
-		loadPanelistProperties();
-	}
+        setupGrid();
+        loadPanelistProperties();
+    }
 
-	private void setupGrid() {
-		Grid.Column<PanelistProperty> nameColumn = grid.addColumn(PanelistProperty::getName).setHeader("Nombre")
-				.setSortable(true).setFlexGrow(1);
+    private void setupGrid() {
+    	 Grid.Column<PanelistProperty> nameColumn = grid.addColumn(PanelistProperty::getName).setHeader("Nombre").setSortable(true).setFlexGrow(1);
 
-		TextField nameFilter = new TextField();
-		nameFilter.setPlaceholder("Filtrar...");
-		nameFilter.setClearButtonVisible(true);
-		nameFilter.addValueChangeListener(e -> {
-			// Logic to update the grid will be here
-		});
+         TextField nameFilter = new TextField();
+         nameFilter.setPlaceholder("Filtrar...");
+         nameFilter.setClearButtonVisible(true);
+         nameFilter.addValueChangeListener(e -> {
+             // Logic to update the grid will be here
+         });
 
-		grid.getHeaderRows().clear();
-		HeaderRow headerRow = grid.appendHeaderRow();
-		headerRow.getCell(nameColumn).setComponent(nameFilter);
+         grid.getHeaderRows().clear();
+         HeaderRow headerRow = grid.appendHeaderRow();
+         headerRow.getCell(nameColumn).setComponent(nameFilter);
 
-		grid.addComponentColumn(property -> {
-			Checkbox checkbox = new Checkbox();
-			Optional<SurveyPropertyMatching> match = surveyPropertyMatchingService.findBySurveyAndProperty(survey,
-					property);
-			match.ifPresent(surveyPropertyMatching -> checkbox.setValue(true));
-			checkboxMap.put(property, checkbox);
-			return checkbox;
-		}).setHeader("Seleccionar").setFlexGrow(0);
+        grid.addComponentColumn(property -> {
+            Checkbox checkbox = new Checkbox();
+            Optional<SurveyPropertyMatching> match = surveyPropertyMatchingService.findBySurveyAndProperty(survey, property);
+            match.ifPresent(surveyPropertyMatching -> checkbox.setValue(true));
+            checkboxMap.put(property, checkbox);
+            return checkbox;
+        }).setHeader("Seleccionar").setFlexGrow(0);
 
 //        grid.addColumn(PanelistProperty::getName).setHeader("Nombre").setSortable(true).setFlexGrow(1);
-		Grid.Column<PanelistProperty> nameColumn = grid.addColumn(PanelistProperty::getName).setHeader("Nombre")
-				.setSortable(true).setFlexGrow(1);
 
-		TextField nameFilter = new TextField();
-		nameFilter.setPlaceholder("Filtrar...");
-		nameFilter.setClearButtonVisible(true);
-		nameFilter.addValueChangeListener(e -> {
-			// Logic to update the grid will be here
-		});
+        grid.addComponentColumn(property -> {
+            TextField textField = new TextField();
+            Optional<SurveyPropertyMatching> match = surveyPropertyMatchingService.findBySurveyAndProperty(survey, property);
+            match.ifPresent(surveyPropertyMatching -> textField.setValue(surveyPropertyMatching.getQuestionLabel()));
+            textFieldMap.put(property, textField);
+            return textField;
+        }).setHeader("Etiqueta").setFlexGrow(1);
+    }
 
-		grid.getHeaderRows().clear();
-		Grid.HeaderRow headerRow = grid.appendHeaderRow();
-		headerRow.getCell(nameColumn).setComponent(nameFilter);
+    private void loadPanelistProperties() {
+        grid.setItems(panelistPropertyService.findAll());
+    }
 
-		grid.addComponentColumn(property -> {
-			TextField textField = new TextField();
-			Optional<SurveyPropertyMatching> match = surveyPropertyMatchingService.findBySurveyAndProperty(survey,
-					property);
-			match.ifPresent(surveyPropertyMatching -> textField.setValue(surveyPropertyMatching.getQuestionLabel()));
-			textFieldMap.put(property, textField);
-			return textField;
-		}).setHeader("Etiqueta").setFlexGrow(1);
-	}
+    private void save() {
+        for (Map.Entry<PanelistProperty, Checkbox> entry : checkboxMap.entrySet()) {
+            PanelistProperty property = entry.getKey();
+            Checkbox checkbox = entry.getValue();
+            TextField textField = textFieldMap.get(property);
 
-	private void loadPanelistProperties() {
-		grid.setItems(panelistPropertyService.findAll());
-	}
-
-	private void save() {
-		for (Map.Entry<PanelistProperty, Checkbox> entry : checkboxMap.entrySet()) {
-			PanelistProperty property = entry.getKey();
-			Checkbox checkbox = entry.getValue();
-			TextField textField = textFieldMap.get(property);
-
-			if (checkbox.getValue()) {
-				Optional<SurveyPropertyMatching> existingMatch = surveyPropertyMatchingService
-						.findBySurveyAndProperty(survey, property);
-				SurveyPropertyMatching match = existingMatch.orElseGet(SurveyPropertyMatching::new);
-				match.setSurvey(survey);
-				match.setProperty(property);
-				match.setQuestionLabel(textField.getValue());
-				surveyPropertyMatchingService.save(match);
-			} else {
-				surveyPropertyMatchingService.findBySurveyAndProperty(survey, property)
-						.ifPresent(match -> surveyPropertyMatchingService.delete(match.getId()));
-			}
-		}
-		close();
-	}
+            if (checkbox.getValue()) {
+                Optional<SurveyPropertyMatching> existingMatch = surveyPropertyMatchingService.findBySurveyAndProperty(survey, property);
+                SurveyPropertyMatching match = existingMatch.orElseGet(SurveyPropertyMatching::new);
+                match.setSurvey(survey);
+                match.setProperty(property);
+                match.setQuestionLabel(textField.getValue());
+                surveyPropertyMatchingService.save(match);
+            } else {
+                surveyPropertyMatchingService.findBySurveyAndProperty(survey, property)
+                        .ifPresent(match -> surveyPropertyMatchingService.delete(match.getId()));
+            }
+        }
+        close();
+    }
 }
